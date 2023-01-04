4ead94b750e73687bfeae83e74214f8da158b330
Prabhu <prabhudattagiri34@gmail.com>
2023-01-04 20:32:21 +0530

    Code Simplify

--- Assignment2/Program5.java
+++ Assignment2/Program5.java
@@ -1,6 +1,4 @@
-
-/*Write a java program compute following series and take a numbers num as 
-input
+/*Write a java program compute following series and take a numbers num as input
  x-x3/3! + x5/5!-x7/7!+------+xn/n!
  where x=sum of all even digits except 2 and 8
  and n= sum of all odd digits except 1 and 3 */
@@ -19,15 +17,16 @@ public class Program5
         while (num > 0) 
         {
             int t = num % 10;
-            if (t % 2 == 0) 
+            if (t % 2 == 0) //Even Number 
             {
-                if (t != 2 && t != 8) 
+                if (t != 2 && t != 8) //Except 2 and 8
                 {
-                    x += t;
+                    x += t; //x = sum of even number except 2 & 8
                 }
-            } else 
+            } 
+            else //Automatically odd
             {
-                if (t != 1 && t != 3) 
+                if (t != 1 && t != 3)
                 {
                     n += t;
                 }
@@ -37,16 +36,14 @@ public class Program5
         result = x;
         for (int i = 3; i <= n; i = i + 2) 
         {
-            long factorialValue = 1;
-            long xToPowerValue = 1;
-            for (int j = 1; j <= i; j++) 
+            long pow=1,fact=1;
+            for(int j=1;j<=i;j++)
             {
-
-                factorialValue *= j;
-                xToPowerValue *= x;
+                pow=pow*x;
+                fact=fact*j;
             }
-            double calculation = (double) xToPowerValue / factorialValue;
-            result = count % 2 == 0 ? result + calculation : result - calculation;
+            double r=(double) pow/fact;
+            result = (count%2==0? result+r: result-r);
             count++;
         }
         System.out.println("The result of the series: " + result);
